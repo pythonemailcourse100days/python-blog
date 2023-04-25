b46a85905b894916ba33ce6adf9bb9927c346460
danika <danika@arktrade.com>
2023-04-25 16:23:21 +0200

    Complete entire Project

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from sqlalchemy.orm.exc import NoResultFound
 from functools import wraps
+import os
+from dotenv import load_dotenv
+
+load_dotenv('./.env')
+API_KEY = os.getenv("MyAPIKey")
 
 
 app = Flask(__name__)
 login_manager = LoginManager()
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = API_KEY
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
