04fac06e4cf496d621ba3985d200ca886f330b66
danika <danika@arktrade.com>
2023-04-25 16:08:15 +0200

    Complete entire Project

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from functools import wraps
 
 app = Flask(__name__)
 login_manager = LoginManager()
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = ''
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
